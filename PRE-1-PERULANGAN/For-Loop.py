a30b16b1fdf0b186f432faf683336d5bdf9edc41
Dodystiawan <botakdede009@gmail.com>
2025-04-14 21:53:27 +0800

    Add files via upload

---
+++ PRE-1-PERULANGAN/For-Loop.py
@@ -0,0 +1,25 @@
+# perulangan (LOOP)
+
+# for loop
+# for kondisi:
+# aksi
+
+a = 10
+a += 1
+print(a)
+a += 1
+print(a)
+a += 1
+print(a)
+
+# perulangan dengan list
+angka_list = [0,1,4,8,10]
+for i in angka_list:
+    print(f"i sekarang ->{i}")
+    
+    print("ini akhir for 1/n")
+   
+    # perulangan str
+    angka_str = "dody"
+    for i in angka_str:
+        print(f"i sekarang ->{i}")
