1005ee8060127824ece5f3e1e5d468c0fc8e70d5
Dodystiawan <botakdede009@gmail.com>
2025-05-26 00:54:43 +0800

    Delete REVISI TUGAS MODUL 2/PRAKTEK 21 directory

--- REVISI TUGAS MODUL 2/PRAKTEK 21/Praktek-1.py
+++
@@ -1,8 +0,0 @@
-# impor library numpy
-import numpy as np
-
-# membuat array dengan numpy
-nilai_siswa = np.array([85, 55, 40, 90])
-
-# akses data pada array
-print(nilai_siswa[3])
--- REVISI TUGAS MODUL 2/PRAKTEK 21/Praktek-4.py
+++
@@ -1,9 +0,0 @@
-# Linear Traversal
-# membuat array
-arr = [1, 2, 3, 4, 5]
-
-# Linear Traversal ke tiap elemen arr
-print("Linear Traversal: ", end=" ")
-for i in arr:
-    print(i, end=" ")
-print()
